d2a514c63c5cb3d58b8c2ea05e52eb0644034a7c
zhouhuang <12345678>
2020-03-07 19:58:36 +0800

    单独

--- chapter1/eureka-server/src/main/java/com/forezp/EurekaServerApplication.java
+++ chapter1/eureka-server/src/main/java/com/forezp/EurekaServerApplication.java
@@ -9,7 +9,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
-//你好呀
+//你好呀帆帆帆帆
 		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 }
